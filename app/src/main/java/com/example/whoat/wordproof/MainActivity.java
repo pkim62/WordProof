97847965ce716ddea8919b63bbe789e445209a99
pkim62@illnois.edu <4321Hana!>
2018-12-03 22:39:22 -0600

    button work pt 1

--- app/src/main/java/com/example/whoat/wordproof/MainActivity.java
+++ app/src/main/java/com/example/whoat/wordproof/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.whoat.wordproof;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //wire button to do stuff
+        //gets button
+        Button btn = (Button) findViewById(R.id.Action);
+        //lets do stuff with button
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("MyApp", "this a magic log message!");
+            }
+        });
     }
 }
