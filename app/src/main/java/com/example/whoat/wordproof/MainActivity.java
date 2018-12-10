3174c0daaedffa2c4ae34479f26462602fb1ae79
pkim62@illnois.edu <4321Hana!>
2018-12-09 23:02:04 -0600

    FINI def

--- app/src/main/java/com/example/whoat/wordproof/MainActivity.java
+++ app/src/main/java/com/example/whoat/wordproof/MainActivity.java
@@ -4,27 +4,37 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.os.AsyncTask;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView t1;
+    EditText e1;
     String url;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        url = dictionaryEntries();
+        t1 = (TextView)findViewById(R.id.txtOutput);
+        e1 = (EditText)findViewById((R.id.input));
     }
     public void requestApiOnClick(View v) {
-        MyApiRequest myApiRequest = new MyApiRequest(this);
-        myApiRequest.execute(url);
+        MyApiRequest myApiRequest = new MyApiRequest(this, t1);
+        myApiRequest.execute(dictionaryEntries());
+    }
+    private String inflections() {
+        final String language = "en";
+        final String word = e1.getText().toString();
+        final String word_id = word.toLowerCase(); //word id is case sensitive and lowercase is required
+        return "https://od-api.oxforddictionaries.com:443/api/v1/inflections/" + language + "/" + word_id;
     }
     private String dictionaryEntries() {
         final String language = "en";
-        final String word = "Eye";
-        final String word_id = word.toLowerCase();
-        return "https://od-api.oxforddictionaries.com:443/api/v1/search/" + language + "?q=" + word_id;
+        final String word = e1.getText().toString();
+        final String word_id = word.toLowerCase(); //word id is case sensitive and lowercase is required
+        return "https://od-api.oxforddictionaries.com:443/api/v1/entries/" + language + "/" + word_id;
     }
 }
--- app/src/main/java/com/example/whoat/wordproof/MyApiRequest.java
+++ app/src/main/java/com/example/whoat/wordproof/MyApiRequest.java
@@ -2,53 +2,85 @@ package com.example.whoat.wordproof;
 import android.content.Context;
 import android.os.AsyncTask;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import javax.net.ssl.HttpsURLConnection;
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+
+import android.os.Handler;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-public class MyApiRequest extends AsyncTask<String,Integer,String> {
-    final String app_id = "0eb99713";
-    final String app_key = "d399079a0bbdadee2d55734d0fcb9fba";
-    String newUrl;
+public class MyApiRequest extends AsyncTask<String, Integer, String> {
     Context c;
-
-    MyApiRequest(Context context) {
-        //thi
-        c = context;
+    EditText e1;
+    TextView t2;
+    MyApiRequest(Context co, TextView t) {
+        c = co;
+        t2 = t;
     }
-@Override
-    protected String doInBackground(String...params) {
-    newUrl = params[0];
-    try {
-        URL url = new URL(newUrl);
-        HttpsURLConnection urlConnection = (HttpsURLConnection) url.openConnection();
-        urlConnection.setRequestProperty("Accept","application/json");
-        urlConnection.setRequestProperty("app_id",app_id);
-        urlConnection.setRequestProperty("app_key",app_key);
+    @Override
+    protected String doInBackground(String... params) {
 
-        // read the output from the server
-        BufferedReader reader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
-        StringBuilder stringBuilder = new StringBuilder();
+        
+        final String app_id =  "0eb99713";
+        final String app_key = "d399079a0bbdadee2d55734d0fcb9fba";
+        try {
+            URL url = new URL(params[0]);
+            HttpsURLConnection urlConnection = (HttpsURLConnection) url.openConnection();
+            urlConnection.setRequestProperty("Accept","application/json");
+            urlConnection.setRequestProperty("app_id",app_id);
+            urlConnection.setRequestProperty("app_key",app_key);
 
-        String line = null;
-        while ((line = reader.readLine()) != null) {
-            stringBuilder.append(line + "\n");
+            // read the output from the server
+            BufferedReader reader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+            StringBuilder stringBuilder = new StringBuilder();
+
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                stringBuilder.append(line + '\n');
+            }
+
+            return stringBuilder.toString();
+
         }
-
-        return stringBuilder.toString();
-
+        catch (IOException e) {
+            e.printStackTrace();
+            return e.toString();
+        }
     }
-    catch (Exception e) {
-        e.printStackTrace();
-        return e.toString();
-    }
-}
-@Override
-    protected void onPostExecute(String s) {
-        super.onPostExecute(s);
-    Toast.makeText(c,s,Toast.LENGTH_SHORT).show();
+
+    @Override
+    protected void onPostExecute(String result) {
+        super.onPostExecute(result);
+
+        String def;
+        try {
+            JSONObject js = new JSONObject(result);
+            JSONArray results = js.getJSONArray("results");
+            JSONObject lentries = results.getJSONObject(0);
+
+            JSONArray la = lentries.getJSONArray("lexicalEntries");
+            JSONObject entries = la.getJSONObject(0);
+
+            JSONArray e = entries.getJSONArray("entries");
+            JSONObject senses = e.getJSONObject(0);
+
+            JSONArray s = senses.getJSONArray("senses");
+            JSONObject d = s.getJSONObject(0);
+
+            JSONArray de = d.getJSONArray("definitions");
+            def = de.getString(0);
+
+            t2.setText(def);
+            Toast.makeText(c,def,Toast.LENGTH_SHORT);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
