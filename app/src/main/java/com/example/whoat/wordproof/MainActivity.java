e5baf81509cc3f56044905b46b3ea544dbc8053a
pkim62@illnois.edu <4321Hana!>
2018-12-11 00:44:18 -0600

    UI work

--- app/src/main/java/com/example/whoat/wordproof/MainActivity.java
+++ app/src/main/java/com/example/whoat/wordproof/MainActivity.java
@@ -2,39 +2,27 @@ package com.example.whoat.wordproof;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.os.AsyncTask;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     TextView t1;
     EditText e1;
-    String url;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        t1 = (TextView)findViewById(R.id.txtOutput);
-        e1 = (EditText)findViewById((R.id.input));
+        t1 = findViewById(R.id.txtOutput);
+        e1 = findViewById((R.id.input));
     }
     public void requestApiOnClick(View v) {
-        MyApiRequest myApiRequest = new MyApiRequest(this, t1);
+        MyApiRequest myApiRequest = new MyApiRequest(this, t1, e1);
         myApiRequest.execute(dictionaryEntries());
     }
-    private String inflections() {
-        final String language = "en";
-        final String word = e1.getText().toString();
-        final String word_id = word.toLowerCase(); //word id is case sensitive and lowercase is required
-        return "https://od-api.oxforddictionaries.com:443/api/v1/inflections/" + language + "/" + word_id;
-    }
     private String dictionaryEntries() {
-        final String language = "en";
         final String word = e1.getText().toString();
-        final String word_id = word.toLowerCase(); //word id is case sensitive and lowercase is required
-        return "https://od-api.oxforddictionaries.com:443/api/v1/entries/" + language + "/" + word_id;
+        final String word_id = word.toLowerCase();
+        return "https://od-api.oxforddictionaries.com:443/api/v1/entries/en/" + word_id;
     }
 }
--- app/src/main/java/com/example/whoat/wordproof/MyApiRequest.java
+++ app/src/main/java/com/example/whoat/wordproof/MyApiRequest.java
@@ -2,13 +2,10 @@ package com.example.whoat.wordproof;
 import android.content.Context;
 import android.os.AsyncTask;
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import javax.net.ssl.HttpsURLConnection;
 
-import android.os.Handler;
-import android.util.Log;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -18,50 +15,47 @@ import org.json.JSONObject;
 
 public class MyApiRequest extends AsyncTask<String, Integer, String> {
     Context c;
-    EditText e1;
     TextView t2;
-    MyApiRequest(Context co, TextView t) {
+    EditText e2;
+    MyApiRequest(Context co, TextView t, EditText e) {
         c = co;
         t2 = t;
+        e2 = e;
     }
+    // Needed api example to help me do this from Oxford Api
+    // (https://developer.oxforddictionaries.com/documentation#!/Dictionary32entries/get_entries_source_lang_word_id)
     @Override
     protected String doInBackground(String... params) {
-
-        
-        final String app_id =  "0eb99713";
-        final String app_key = "d399079a0bbdadee2d55734d0fcb9fba";
+        final String myAppId =  "0eb99713";
+        final String myAppKey = "d399079a0bbdadee2d55734d0fcb9fba";
         try {
-            URL url = new URL(params[0]);
-            HttpsURLConnection urlConnection = (HttpsURLConnection) url.openConnection();
+            URL myURL = new URL(params[0]);
+            HttpsURLConnection urlConnection = (HttpsURLConnection) myURL.openConnection();
             urlConnection.setRequestProperty("Accept","application/json");
-            urlConnection.setRequestProperty("app_id",app_id);
-            urlConnection.setRequestProperty("app_key",app_key);
-
-            // read the output from the server
+            urlConnection.setRequestProperty("app_id",myAppId);
+            urlConnection.setRequestProperty("app_key",myAppKey);
             BufferedReader reader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
             StringBuilder stringBuilder = new StringBuilder();
-
-            String line = null;
+            String line;
             while ((line = reader.readLine()) != null) {
                 stringBuilder.append(line + '\n');
             }
-
             return stringBuilder.toString();
-
         }
-        catch (IOException e) {
+        catch (Exception e) {
+            String a = "Not a real word, Please try again!";
+            t2.setText(a);
             e.printStackTrace();
             return e.toString();
         }
     }
-
     @Override
-    protected void onPostExecute(String result) {
-        super.onPostExecute(result);
-
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+        String realWord = e2.getText().toString() + " is a Real Word and the Definition is: ";
         String def;
         try {
-            JSONObject js = new JSONObject(result);
+            JSONObject js = new JSONObject(s);
             JSONArray results = js.getJSONArray("results");
             JSONObject lentries = results.getJSONObject(0);
 
@@ -71,13 +65,13 @@ public class MyApiRequest extends AsyncTask<String, Integer, String> {
             JSONArray e = entries.getJSONArray("entries");
             JSONObject senses = e.getJSONObject(0);
 
-            JSONArray s = senses.getJSONArray("senses");
-            JSONObject d = s.getJSONObject(0);
+            JSONArray se = senses.getJSONArray("senses");
+            JSONObject d = se.getJSONObject(0);
 
             JSONArray de = d.getJSONArray("definitions");
             def = de.getString(0);
 
-            t2.setText(def);
+            t2.setText(realWord + def);
             Toast.makeText(c,def,Toast.LENGTH_SHORT);
         } catch (JSONException e) {
             e.printStackTrace();
